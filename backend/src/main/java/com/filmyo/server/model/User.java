cb8ac5f88ddd3ae7a07247c50e84628f27fcc17b
Alejandro Gomez <alejgomez4@gmail.com>
2025-06-09 03:36:30 +0200

    Clase Users Realizada Y Pom
    He realizado la clase users para que se añadan sus valores a la tabal de datos, algo asi como SQL, ademas IntelliJ no estaba reconociendo que el proyecto era Java asi que he usado Pom y le he configurado como Maven

---
+++ backend/src/main/java/com/filmyo/server/model/User.java
@@ -0,0 +1,69 @@
+package com.filmyo.server.model;
+
+import jakarta.persistence.*;
+
+//Podriamos usar LoomBot pero es mejor entender todo esto
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    //Properties
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    //Builders
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    //Getters & Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- backend/src/test/java/com/filmyo/server/ApiApplicationTests.java
+++ backend/src/test/java/com/filmyo/server/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.filmyo.api;
+package com.filmyo.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
